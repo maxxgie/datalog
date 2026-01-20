c394aaed090996d880d25fbc47e96faec068a642
maxxgie <youngmcnjane@gmail.com>
2026-01-20 08:47:11 +0300

    corect

--- kb_facts.py
+++ kb_facts.py
@@ -1,45 +1,47 @@
 from pyDatalog import pyDatalog
 
 def load_facts():
+    assert_fact = pyDatalog.assert_fact
+
     # ---- PESTS ----
-    + pyDatalog.terms.pest('avocado_thrips')
-    + pyDatalog.terms.pest('boring_beetles')
-    + pyDatalog.terms.pest('ambrosia_beetles')
-    + pyDatalog.terms.pest('polyphagous_shothole_borer')
-    + pyDatalog.terms.pest('avocado_lace_bug')
-    + pyDatalog.terms.pest('mites')
-    + pyDatalog.terms.pest('persea_mite')
-    + pyDatalog.terms.pest('avocado_brown_mite')
-    + pyDatalog.terms.pest('sixspotted_mite')
-    + pyDatalog.terms.pest('caterpillars')
-    + pyDatalog.terms.pest('scale_insects')
-    + pyDatalog.terms.pest('greenhouse_thrips')
+    assert_fact('pest', 'avocado_thrips')
+    assert_fact('pest', 'boring_beetles')
+    assert_fact('pest', 'ambrosia_beetles')
+    assert_fact('pest', 'polyphagous_shothole_borer')
+    assert_fact('pest', 'avocado_lace_bug')
+    assert_fact('pest', 'mites')
+    assert_fact('pest', 'persea_mite')
+    assert_fact('pest', 'avocado_brown_mite')
+    assert_fact('pest', 'sixspotted_mite')
+    assert_fact('pest', 'caterpillars')
+    assert_fact('pest', 'scale_insects')
+    assert_fact('pest', 'greenhouse_thrips')
 
     # ---- DISEASES ----
-    + pyDatalog.terms.disease('phytophthora_root_rot')
-    + pyDatalog.terms.disease('laurel_wilt')
-    + pyDatalog.terms.disease('anthracnose')
-    + pyDatalog.terms.disease('cercospora_spot')
+    assert_fact('disease', 'phytophthora_root_rot')
+    assert_fact('disease', 'laurel_wilt')
+    assert_fact('disease', 'anthracnose')
+    assert_fact('disease', 'cercospora_spot')
 
     # ---- CHEMICALS ----
-    + pyDatalog.terms.insecticide('abamectin')
-    + pyDatalog.terms.insecticide('spinosad')
-    + pyDatalog.terms.insecticide('spinetoram')
-    + pyDatalog.terms.insecticide('imidacloprid')
-    + pyDatalog.terms.miticide('spirodiclofen')
-    + pyDatalog.terms.fungicide('copper')
-    + pyDatalog.terms.fungicide('azoxystrobin')
+    assert_fact('insecticide', 'abamectin')
+    assert_fact('insecticide', 'spinosad')
+    assert_fact('insecticide', 'spinetoram')
+    assert_fact('insecticide', 'imidacloprid')
+    assert_fact('miticide', 'spirodiclofen')
+    assert_fact('fungicide', 'copper')
+    assert_fact('fungicide', 'azoxystrobin')
 
     # ---- ASSOCIATIONS ----
-    + pyDatalog.terms.controls_pest('abamectin', 'avocado_thrips')
-    + pyDatalog.terms.controls_pest('spinosad', 'avocado_thrips')
-    + pyDatalog.terms.controls_pest('spirodiclofen', 'persea_mite')
+    assert_fact('controls_pest', 'abamectin', 'avocado_thrips')
+    assert_fact('controls_pest', 'spinosad', 'avocado_thrips')
+    assert_fact('controls_pest', 'spirodiclofen', 'persea_mite')
 
-    + pyDatalog.terms.controls_disease('copper', 'anthracnose')
-    + pyDatalog.terms.controls_disease('azoxystrobin', 'anthracnose')
+    assert_fact('controls_disease', 'copper', 'anthracnose')
+    assert_fact('controls_disease', 'azoxystrobin', 'anthracnose')
 
     # ---- PROPERTIES ----
-    + pyDatalog.terms.has_irac_group('abamectin', '6')
-    + pyDatalog.terms.has_frac_group('azoxystrobin', '11')
-    + pyDatalog.terms.is_systemic('imidacloprid')
-    + pyDatalog.terms.is_organic('spinosad')
+    assert_fact('has_irac_group', 'abamectin', '6')
+    assert_fact('has_frac_group', 'azoxystrobin', '11')
+    assert_fact('is_systemic', 'imidacloprid')
+    assert_fact('is_organic', 'spinosad')
