393ff65e978a68eb904475bbf987a5dc1343bf61
maxxgie <youngmcnjane@gmail.com>
2026-01-20 10:57:20 +0300

    j

--- kb_rules.py
+++ kb_rules.py
@@ -1,22 +1,16 @@
 from pyDatalog import pyDatalog
 
-# Create all terms needed for rules
+# Terms already created in kb_loader.py, but just in case
 pyDatalog.create_terms("""
     Pest, Chemical, Disease, Tool, Control,
     find_pest_control, find_disease_control,
     is_ipm_choice, is_biological_or_natural,
-    is_ipm_tool, natural_solution
+    is_ipm_tool, natural_solution,
+    pest, disease, controls_pest, controls_disease
 """)
 
-# --- RULES (module-level) ---
-# Pest -> Control
-find_pest_control(Pest, Chemical) <= pyDatalog.pest(Pest) & pyDatalog.controls_pest(Chemical, Pest)
-
-# Disease -> Control
-find_disease_control(Disease, Chemical) <= pyDatalog.disease(Disease) & pyDatalog.controls_disease(Chemical, Disease)
-
-# IPM tools
-is_ipm_choice(Tool) <= pyDatalog.is_ipm_tool(Tool)
-
-# Biological / Natural
-is_biological_or_natural(Control) <= pyDatalog.natural_solution(Control)
+# RULES (module-level)
+find_pest_control(Pest, Chemical) <= pest(Pest) & controls_pest(Chemical, Pest)
+find_disease_control(Disease, Chemical) <= disease(Disease) & controls_disease(Chemical, Disease)
+is_ipm_choice(Tool) <= is_ipm_tool(Tool)
+is_biological_or_natural(Control) <= natural_solution(Control)
